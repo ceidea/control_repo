3f941928086ac7b592d6d980870d925212fd101a
ceidea <89033939+ceidea@users.noreply.github.com>
2021-08-18 17:26:23 -0500

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -4,7 +4,7 @@ node 'uxtdapl7.uth.tmc.edu' {
   include role::master_server
   file { '/root/README':
     ensure => file,
-    content => "This machine is really ${fqdn}\n",
+    content => "This machine is really pretty standard\n",
   }
 }
 node /^myweb/ {
