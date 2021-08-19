bd8cca21d8d5bc3a93dedda3ec05e8721e2ad396
ceidea <89033939+ceidea@users.noreply.github.com>
2021-08-19 10:14:18 -0500

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -2,6 +2,10 @@ node default {
 }
 node 'uxtdapl7.uth.tmc.edu' {
   include role::master_server
+  file {'/root/README':
+    ensure => file,
+    content => "This ${os.name} is ${fqdn}\n",
+  }
 }
 node /^myweb/ {
   include role::app_server
