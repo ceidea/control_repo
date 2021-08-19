9891bf8dfb8ddb4bb0646e92a0c9ac44c607d790
ceidea <89033939+ceidea@users.noreply.github.com>
2021-08-19 15:59:04 -0500

    Update agent_nodes.pp

--- site/profile/manifests/agent_nodes.pp
+++ site/profile/manifests/agent_nodes.pp
@@ -2,6 +2,7 @@ class profile::agent_nodes {
   include dockeragent
   dockeragent::node {'myweb.uth.tmc.edu':}
   dockeragent::node {'mydb.uth.tmc.edu':}
+  dockeragent::node {'mynecraft':}
   host { 'mydb.uth.tmc.edu':
     ensure => present,
     ip     => '172.18.0.2',
