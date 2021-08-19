d130535d3fb1ede23d91275ddffff39870fd2918
ceidea <89033939+ceidea@users.noreply.github.com>
2021-08-19 11:32:45 -0500

    Update ssh_server.pp

--- site/profile/manifests/ssh_server.pp
+++ site/profile/manifests/ssh_server.pp
@@ -10,6 +10,6 @@ class profile::ssh_server {
     ensure => present,
     user   => 'root',
     type   => 'ssh-rsa',
-    key    => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQDN9jcp4cqXQMjPhLf+Yk9fyS+Z0PTJrSjuraJN6PBnxAKfoccKfmYZG1rIs0P7fGANQproNepEm2ow/JbQE3Ngguh5JMRwmkfNJP7tA8RZcdynFTDqKcN/a+mnSUbogjjI3kUk3pVyMZ4zkdUhCZ2Ph2aNb+ahnEgvIjqyuQNaoQqJCzB2/MVEponOfkBNXC+A4fperN2Os36yrCEbc/JibeWtnkx2ZKc2aaLtKpEI8PeeEv6yUFnsgQ49+krXEfqHTulHwygSnxNmddLZ8tgzMp0KnlQ8h9DsApfxdEd6pr9F0qZhxqaRYe/8BiJ03vNnILtY7DTiuRdvj0Ems69F'
+    key    => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQDN9jcp4cqXQMjPhLf+Yk9fyS+Z0PTJrSjuraJN6PBnxAKfoccKfmYZG1rIs0P7fGANQproNepEm2ow/JbQE3Ngguh5JMRwmkfNJP7tA8RZcdynFTDqKcN/a+mnSUbogjjI3kUk3pVyMZ4zkdUhCZ2Ph2aNb+ahnEgvIjqyuQNaoQqJCzB2/MVEponOfkBNXC+A4fperN2Os36yrCEbc/JibeWtnkx2ZKc2aaLtKpEI8PeeEv6yUFnsgQ49+krXEfqHTulHwygSnxNmddLZ8tgzMp0KnlQ8h9DsApfxdEd6pr9F0qZhxqaRYe/8BiJ03vNnILtY7DTiuRdvj0Ems69F',
   }
 }
