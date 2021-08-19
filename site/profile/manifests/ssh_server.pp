b95a5dfe68afe00c6b9fc70752b9239abbd545c5
ceidea <89033939+ceidea@users.noreply.github.com>
2021-08-19 10:42:53 -0500

    Create ssh_server.pp

---
+++ site/profile/manifests/ssh_server.pp
@@ -0,0 +1,15 @@
+class profile::ssh_server {
+  package { 'openssh-server':
+    ensure => present,
+  }
+  service { 'sshd':
+    ensure => 'running',
+    enable => 'true',
+  }
+  ssh_authorized_key { 'root@uxtdapl7.uth.tmc.edu':
+    ensure => present,
+    user   => 'root',
+    type   => 'ssh-rsa',
+    key    => "${ssh[rsa[key]]"
+  }
+}
